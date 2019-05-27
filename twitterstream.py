5d39f13c70151b507bbb9952a00b143703a05525
Parker Hall <40338216+NerdParker@users.noreply.github.com>
2019-05-26 20:46:52 -0400

    Update twitterstream.py

--- twitterstream.py
+++ twitterstream.py
@@ -2,10 +2,10 @@ import requests
 from requests_oauthlib import OAuth1
 import jsonlines
 
-api_key = "NABkOiCt1RCrHb9VUPQ6T4obk"
-api_secret = "Ez8eXJDMC26hKSxtdwATk24arQrsb6OO9ITXg9MptKRLgaVjUC"
-access_token_key = "1129107025852547073-99ZPjAtBdE1Wk344pxBz3DEaQwnxfr"
-access_token_secret = "SoRZXueMcACGOHZwXd50M9HwybrPCzWM72bA6Drg2Q6rD"
+api_key = "XXXXXXXXXXXXXXXXXXXXX"
+api_secret = "XXXXXXXXXXXXXXXXXXXXX"
+access_token_key = "XXXXXXXXXXXXXXXXXXXXX"
+access_token_secret = "XXXXXXXXXXXXXXXXXXXXX"
 
 url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
 auth = OAuth1(api_key, api_secret, access_token_key, access_token_secret)
